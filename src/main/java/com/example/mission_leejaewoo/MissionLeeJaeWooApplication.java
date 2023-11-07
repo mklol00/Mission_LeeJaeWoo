583fba5bab67e830e5088c8561f1066222ef5ee4
mklol00 <dlwodn97@naver.com>
2023-11-07 10:07:38 +0900

    init

---
+++ src/main/java/com/example/mission_leejaewoo/MissionLeeJaeWooApplication.java
@@ -0,0 +1,13 @@
+package com.example.mission_leejaewoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionLeeJaeWooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionLeeJaeWooApplication.class, args);
+	}
+
+}
