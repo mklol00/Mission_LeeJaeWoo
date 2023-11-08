28e280b349d5444cc463595e2c9f97183bc18548
mklol00 <dlwodn97@naver.com>
2023-11-08 16:06:17 +0900

    1week

---
+++ src/main/java/com/example/mission_leejaewoo/Rq.java
@@ -0,0 +1,55 @@
+package com.example.mission_leejaewoo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rq {
+    String cmd;
+    String action;
+    String queryString;
+    Map<String, String> paramsMap;
+
+    Rq(String cmd) {
+        paramsMap = new HashMap<>();
+
+        this.cmd = cmd;
+
+        String[] cmdBits = cmd.split("\\?", 2);
+        action = cmdBits[0].trim();
+
+        if (cmdBits.length == 1) {
+            return;
+        }
+
+        queryString = cmdBits[1].trim();
+
+        String[] queryStringBits = queryString.split("&");
+
+        for (int i = 0; i < queryStringBits.length; i++) {
+            String queryParamStr = queryStringBits[i];
+            String[] queryParamStrBits = queryParamStr.split("=", 2);
+
+            String paramName = queryParamStrBits[0];
+            String paramValue = queryParamStrBits[1];
+
+            paramsMap.put(paramName, paramValue);
+        }
+    }
+
+    String getAction() {
+        return action;
+    }
+
+    public int getParamAsInt(String paramName, int defaultValue) {
+        String paramValue = paramsMap.get(paramName);
+
+        if (paramValue != null) {
+            try {
+                return Integer.parseInt(paramValue);
+            } catch (NumberFormatException e) {
+            }
+        }
+
+        return defaultValue;
+    }
+}
